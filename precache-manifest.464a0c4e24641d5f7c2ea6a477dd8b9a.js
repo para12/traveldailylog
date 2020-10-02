b7dd7bbe0a4c5adbd61e79e545da24090c303065
hyunwoo lee <hyunwooda@gmail.com>
2020-10-03 01:21:11 +0900

    Updates

---
+++ precache-manifest.464a0c4e24641d5f7c2ea6a477dd8b9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "167068d93bd98b170b46bd0f5794a148",
+    "url": "/traveldailylog/index.html"
+  },
+  {
+    "revision": "a4a2e83f5570a107c764",
+    "url": "/traveldailylog/static/css/main.7da6dd3a.chunk.css"
+  },
+  {
+    "revision": "c866d2257d2ecb1378c7",
+    "url": "/traveldailylog/static/js/2.dc1c3e01.chunk.js"
+  },
+  {
+    "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
+    "url": "/traveldailylog/static/js/2.dc1c3e01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4a2e83f5570a107c764",
+    "url": "/traveldailylog/static/js/main.2ae005c7.chunk.js"
+  },
+  {
+    "revision": "38eb424d2d73015da5c6",
+    "url": "/traveldailylog/static/js/runtime-main.0db95590.js"
+  }
+]);
